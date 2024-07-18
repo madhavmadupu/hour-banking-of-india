7625040eb533d8c938e8d732ca044364f0c0d40a
madhavmadupu <madhav.madupu@gmail.com>
2024-07-18 10:39:12 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='flex flex-row justify-between px-[2rem] py-[1rem] border-b-2'>
+    <div className='flex flex-row justify-between px-[2rem] py-[0.5rem] border-b-2'>
         {/* Logo */}
         <div className='flex flex-row justify-center items-center gap-[1rem]'>
             <img src={"/assets/logo_placeholder.svg"} alt="logo" />
@@ -11,7 +11,7 @@ const Navbar = () => {
         {/* NavList */}
         <div className='flex flex-row justify-center items-center gap-[2rem] text-[#625B71]'>
           {/* Pages */}
-          <div className=' flex flex-row gap-[2rem] border-r-2 px-[2rem] py-[1rem]'>
+          <div className=' flex flex-row gap-[2rem] border-r-2 px-[2rem] py-[0.5rem]'>
             <div className='flex flex-col justify-center items-center'>
               <img src="/assets/icons/marketplace.svg" alt="icon" />
               <p className='text-[12px] font-medium'>Marketplace</p>
@@ -34,7 +34,7 @@ const Navbar = () => {
           {/* CTA */}
           <div className='flex flex-row gap-[2rem] justify-center items-center'>
             <a href="/" className='font-semibold'>Join Now</a>
-            <a href="/" className='font-semibold text-white bg-[#605DFA] px-[2rem] py-[1rem] rounded-full'>Sign In</a>
+            <a href="/" className='font-semibold text-white bg-[#605DFA] px-[2rem] py-[0.5rem] rounded-full'>Sign In</a>
           </div>
         </div>
     </div>
